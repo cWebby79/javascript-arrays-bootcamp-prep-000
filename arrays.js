2d796068ec2e77c70d61679cbe5492ddb0b1fcac
Chris Webster <cgw57@hotmail.com>
2018-07-13 13:35:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,5 +37,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  retutn array.slice(0,array.length -1);
+  return array.slice(0,array.length -1);
 }
