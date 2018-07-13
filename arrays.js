c7d853985bfcf557d470d68ac2a7bce0fe81823b
Chris Webster <cgw57@hotmail.com>
2018-07-13 13:23:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,7 @@ function addElementToEndOfArray(array, element) {
 function destructivelyAddElementToEndOfArray(array,element) {
   array.push(element);
 }
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
