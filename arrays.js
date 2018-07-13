408b9bc5f0f15723029f3132e2afe3befebd176e
Chris Webster <cgw57@hotmail.com>
2018-07-13 12:52:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(a,e) {
   var newArray = [a];
-  ["e", ...a];
+  ["e", ...newArray];
 }
 
 function destructivelyAddElementToBeginningOfArray(a,e) {
