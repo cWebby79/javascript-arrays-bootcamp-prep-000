0a2333b70eda6fe8d01bc38364de4a7ec5c4801f
Chris Webster <cgw57@hotmail.com>
2018-07-13 13:31:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,9 @@ function destructivelyRemoveElementFromBeginningOfArray(array,element) {
   array.shift(element);
   return array;
 }
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
+  return array;
+}
+
